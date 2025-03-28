55dff5dc9854b6fcf5e6109d151188959cc9be5a
GustavoDiaso <kingclash1906@gmail.com>
2025-03-28 19:58:51 -0300

    criando a classe da janela de cadastro

--- CSS/css.py
+++ CSS/css.py
@@ -2,3 +2,8 @@ main_window = """
     background-color: white;
     
 """
+
+registration_forms = """
+    background-color: blue;
+    border-radius: 30px;
+"""
--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-from PySide6 import  QtWidgets, QtCore, QtGui
+from PySide6 import QtWidgets, QtCore, QtGui
 from CSS import css
-from pathlib import  Path
+from pathlib import Path
 import sqlite3
-import  database.prepared_sql_queries as psq
+import database.prepared_sql_queries as psq
 from database.prepared_sql_queries import register_new_driver
 
-DATABASE_PATH = Path(__file__).parent / 'database/database.sqlite3'
+DATABASE_PATH = Path(__file__).parent / "database/database.sqlite3"
 connection = sqlite3.connect(DATABASE_PATH)
 
 psq.create_table_drivers(connection=connection)
 psq.create_table_vehicles(connection=connection)
 
-class Main_Window(QtWidgets.QWidget):
+
+class MainWindow(QtWidgets.QWidget):
     def __init__(self):
-        super(Main_Window, self).__init__()
+        super(MainWindow, self).__init__()
         self.setStyleSheet(css.main_window)
 
         user_screen = QtGui.QGuiApplication.primaryScreen()
         user_screen_geometry = user_screen.availableGeometry()
         self.setMinimumSize(user_screen_geometry.width(), user_screen_geometry.height())
 
+        registration_forms = RegistrationForms(parent=self)
 
-register_new_driver(connection=connection, psq.Driver())
+
+class RegistrationForms(QtWidgets.QLabel):
+    def __init__(self, parent: MainWindow):
+        super(RegistrationForms, self).__init__(parent=parent)
+        self.setStyleSheet(css.registration_forms)
+
+        user_screen = QtGui.QGuiApplication.primaryScreen()
+        user_screen_geometry = user_screen.availableGeometry()
+        self.setMinimumSize(600, user_screen_geometry.height() * (4.3 / 5))
+
+        self.move(30, (parent.height() // 2 - self.height() // 2) - 10)
+
 
 connection.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QtWidgets.QApplication()
-    main_window = Main_Window()
+    main_window = MainWindow()
     main_window.showMaximized()
     app.exec()
-
-
