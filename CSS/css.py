2fc022e549689122848060d2d0b5bffea21807a9
TheGustavoDiaso <165075774+GustavoDiaso@users.noreply.github.com>
2025-03-31 14:48:31 -0300

    continuando a fazer o forms de cadastro

--- CSS/css.py
+++ CSS/css.py
@@ -4,7 +4,7 @@ main_window = """
 """
 
 registration_forms = """
-    background-color: blue;
+    background-color: #303030;
     color: white;
     font-family: Segoe UI;
     font-weight: 600;
@@ -22,10 +22,15 @@ registration_label_guide = """
 registration_input = """
     border-bottom: 1px solid white;
     border-radius: none;
-    background-color: #0230ec;
+    color: grey;
+    font-size: 16px;
+"""
+registration_input_focused = """
+    border-bottom: 1px solid white;
+    border-radius: none;
     color: white;
+    font-size: 16px;
 """
 
 driver_informations_section = """
-    background-color: blue;
 """
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from PySide6 import QtWidgets, QtCore, QtGui
+from win32api import mouse_event
+
 from CSS import css
 from pathlib import Path
 import sqlite3
@@ -63,7 +65,7 @@ class RegistrationForms(QtWidgets.QLabel):
         )
 
         input_fullname = QtWidgets.QLineEdit(parent=driver_informations_section)
-        input_fullname.setStyleSheet(css.registration_input)
+        input_fullname.setStyleSheet(css.registration_input_focused)
         input_fullname.setFixedSize(inputs_width, inputs_height)
         input_fullname.setMaxLength(100)
         input_fullname.move(
@@ -75,18 +77,92 @@ class RegistrationForms(QtWidgets.QLabel):
         lbl_cpf.setStyleSheet(css.registration_label_guide)
         lbl_cpf.move(
             margin_left,
-            input_fullname.y() + inputs_height + (space_between_elements * 5),
+            input_fullname.y() + inputs_height + (space_between_elements * 6),
         )
 
-        # title = QtWidgets.QLabel("Cadastro de veículo", parent=self)
-        # title.setFixedWidth(315)
-        # title.setStyleSheet(css.registration_title)
-        # title.move(self.width() // 2 - title.width() // 2, 40)
+        input_cpf = QtWidgets.QLineEdit(parent=driver_informations_section)
+        input_cpf.setStyleSheet(css.registration_input_focused)
+        input_cpf.setFixedSize(inputs_width, inputs_height)
+        input_cpf.setMaxLength(14)
+        input_cpf.move(
+            margin_left,
+            lbl_cpf.y() + lbl_cpf.height() + space_between_elements
+        )
 
-        # title = QtWidgets.QLabel("Cadastro de veículo", parent=self)
-        # title.setFixedWidth(315)
-        # title.setStyleSheet(css.registration_title)
-        # title.move(self.width() // 2 - title.width() // 2, 40)
+        lbl_birth_date = QtWidgets.QLabel("Data de nascimento:", parent=driver_informations_section)
+        lbl_birth_date.setStyleSheet(css.registration_label_guide)
+        lbl_birth_date.move(
+            margin_left,
+            input_cpf.y() + inputs_height + (space_between_elements * 6),
+        )
+
+        input_birth_date = QtWidgets.QDateEdit(parent=driver_informations_section)
+        input_birth_date.setStyleSheet(css.registration_input_focused)
+        input_birth_date.setFixedSize(inputs_width, inputs_height)
+        input_birth_date.move(
+            margin_left,
+            lbl_birth_date.y() + lbl_birth_date.height() + space_between_elements
+        )
+
+        # Yes, I know it would be better to separate the adress into street, number and neighborhood
+        # This is just a simple project to show my abilities
+        lbl_address = QtWidgets.QLabel("Endereço:", parent=driver_informations_section)
+        lbl_address.setStyleSheet(css.registration_label_guide)
+        lbl_address.move(
+            margin_left,
+            input_birth_date.y() + inputs_height + (space_between_elements * 6),
+        )
+
+        input_address = QtWidgets.QLineEdit(parent=driver_informations_section)
+        input_address.setStyleSheet(css.registration_input_focused)
+        input_address.setFixedSize(inputs_width, inputs_height)
+        input_address.setMaxLength(150)
+        input_address.move(
+            margin_left,
+            lbl_address.y() + lbl_address.height() + space_between_elements
+        )
+
+        lbl_phone = QtWidgets.QLabel("Telefone:", parent=driver_informations_section)
+        lbl_phone.setStyleSheet(css.registration_label_guide)
+        lbl_phone.move(
+            margin_left,
+            input_address.y() + inputs_height + (space_between_elements * 6),
+        )
+
+        input_ddd = QtWidgets.QLineEdit("DDD",parent=driver_informations_section)
+        input_ddd.setStyleSheet(css.registration_input)
+        input_ddd.setFixedSize(70, inputs_height)
+        input_ddd.setMaxLength(3)
+        input_ddd.move(
+            margin_left,
+            lbl_phone.y() + lbl_phone.height() + space_between_elements
+        )
+        input_ddd.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter)
+
+        # Quando pressionado, um widget chama automaticamente o metodo interno mousePressEvent(event), passando
+        # o evento ocorrido como argumento. O que eu estou fazendo aqui é basicamente sobrescrever o metodo
+        # mousePressEvent original, atribuindo a esse identificador uma nova função.
+        input_ddd.mousePressEvent = lambda event: self.input_toggle_placeholder(input_ddd, "DDD")
+        input_ddd.focusOutEvent = lambda event: self.input_toggle_placeholder(input_ddd, "DDD")
+
+        input_number = QtWidgets.QLineEdit(parent=driver_informations_section)
+        input_number.setStyleSheet(css.registration_input_focused)
+        input_number.setFixedSize(inputs_width - margin_left - input_ddd.width(), inputs_height)
+        input_number.setMaxLength(9)
+        input_number.move(
+            (margin_left*2) + input_ddd.width(),
+            lbl_phone.y() + lbl_phone.height() + space_between_elements
+        )
+
+    def input_toggle_placeholder(self, target_input, placeholder):
+        if target_input.text() == placeholder:
+            target_input.setText("")
+            target_input.setReadOnly(False)
+            target_input.setStyleSheet(css.registration_input_focused)
+        else:
+            target_input.setText(placeholder)
+            target_input.setReadOnly(True)
+            target_input.setStyleSheet(css.registration_input)
 
 
 connection.close()
